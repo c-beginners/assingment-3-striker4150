ce3629988730a748006f50c5c33284136b12e6f4
Darren Kam <darrenmkam4150@gmail.com>
2019-05-09 00:56:59 -0400

    Add print and transpose array methods

--- matrix.cpp
+++ matrix.cpp
@@ -11,6 +11,36 @@ T diagonal_sum(const T (&arr)[rows][cols]) {
     return sum;
 }
 
+template <typename T, size_t rows, size_t cols>
+void print_arr(const T (&arr)[rows][cols]) {
+    for(int row = 0; row < rows; row++) {
+        for(int col = 0; col < cols; col++) {
+            std::cout << arr[row][col] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+template <typename T, size_t rows, size_t cols>
+void transpose_arr(const T (&arr)[rows][cols], T (&output)[cols][rows]) {
+    for(int row = 0; row < rows; row++) {
+        for(int col = 0; col < cols; col++) {
+            output[col][row] = arr[row][col];
+        }
+    }
+}
+
 int main() {
+    int arr[3][2] = {{1,2},{3,4},{5,6}}, transpose[2][3];
+
+    std::cout << "Original array:" << std::endl;
+    print_arr(arr);
+
+    std::cout << "Diagonal sum: " << diagonal_sum(arr) << std::endl;
+
+    transpose_arr(arr, transpose);
+    std::cout << "Transposed array:" << std::endl;
+    print_arr(transpose);
+
     return 0;
 }
