c79c711571301a9ea5ac648a7948591aaea075db
Darren Kam <darrenmkam4150@gmail.com>
2019-05-08 22:57:04 -0400

    Add matrix diagonal sum function

--- matrix.cpp
+++ matrix.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+template <typename T, size_t rows, size_t cols>
+T diagonal_sum(const T (&arr)[rows][cols]) {
+    T sum = 0;
+
+    for(int i = 0; i < (rows < cols ? rows : cols); i++) {
+        sum += arr[i][i];
+    }
+
+    return sum;
+}
+
 int main() {
     return 0;
 }
