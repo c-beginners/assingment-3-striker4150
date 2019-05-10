fdecc00570ba2d64805b1891186049f0b369337e
Darren Kam <darrenmkam4150@gmail.com>
2019-05-10 12:35:34 -0400

    Handle custom user input

--- matrix.cpp
+++ matrix.cpp
@@ -1,46 +1,80 @@
 #include <iostream>
 
-template <typename T, size_t size>
-T diagonal_sum(const T (&arr)[size][size]) {
-    T sum = 0;
+template <typename T>
+int diagonal_sum(const T &mat, int size) {
+    int sum = 0;
 
     for(int i = 0; i < size; i++) {
-        sum += arr[i][i];
+        sum += mat[i][i];
     }
 
     return sum;
 }
 
-template <typename T, size_t size>
-void print_arr(const T (&arr)[size][size]) {
+template <typename T>
+void print_mat(const T &mat, int size) {
     for(int row = 0; row < size; row++) {
         for(int col = 0; col < size; col++) {
-            std::cout << arr[row][col] << " ";
+            std::cout << mat[row][col] << " ";
         }
         std::cout << std::endl;
     }
 }
 
-template <typename T, size_t size>
-void transpose_arr(const T (&arr)[size][size], T (&output)[size][size]) {
+template <typename T>
+void transpose_mat(const T &mat, T &output, int size) {
     for(int row = 0; row < size; row++) {
         for(int col = 0; col < size; col++) {
-            output[col][row] = arr[row][col];
+            output[col][row] = mat[row][col];
         }
     }
 }
 
 int main() {
-    int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}}, transpose[3][3];
+    int **mat, **transpose, size;
 
-    std::cout << "Original array:" << std::endl;
-    print_arr(arr);
+    std::cout << "Enter an matrix size: ";
+    std::cin >> size;
 
-    std::cout << "Diagonal sum: " << diagonal_sum(arr) << std::endl;
+    // Dynamically allocate memory for matrix
+    mat = (int**)calloc(size, sizeof(int*));
+    for(int i = 0; i < size; i++) {
+        mat[i] = (int*)calloc(size, sizeof(int));
+    }
 
-    transpose_arr(arr, transpose);
-    std::cout << "Transposed array:" << std::endl;
-    print_arr(transpose);
+    // Dynamically allocate memory for transposed matrix
+    transpose = (int**)calloc(size, sizeof(int*));
+    for(int i = 0; i < size; i++) {
+        transpose[i] = (int*)calloc(size, sizeof(int));
+    }
+
+    std::cout << "Enter the matrix data:" << std::endl;
+    for(int row = 0; row < size; row++) {
+        for(int col = 0; col < size; col++) {
+            std::cin >> mat[row][col];
+        }
+    }
+
+    std::cout << "Original matrix:" << std::endl;
+    print_mat(mat, size);
+
+    std::cout << "Diagonal sum: " << diagonal_sum(mat, size) << std::endl;
+
+    transpose_mat(mat, transpose, size);
+    std::cout << "Transposed matrix:" << std::endl;
+    print_mat(transpose, size);
+
+    // Free matrix
+    for(int i = 0; i < size; i++) {
+        free(mat[i]);
+    }
+    free(mat);
+
+    // Free transposed matrix
+    for(int i = 0; i < size; i++) {
+        free(transpose[i]);
+    }
+    free(transpose);
 
     return 0;
 }
